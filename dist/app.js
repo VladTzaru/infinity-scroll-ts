20eb29eb236c7020f1826abcbbd03700877fbb59
VladTzaru <bvanja.87@gmail.com>
2020-12-07 16:46:12 +0100

    Added getQuote, renderText and tweetQuote methods

--- dist/app.js
+++ dist/app.js
@@ -8,16 +8,46 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+const quoteContainerEl = document.getElementById('quote-container');
+const quoteEl = document.getElementById('quote');
+const authorEl = document.getElementById('author');
+const twitterBtnEl = document.getElementById('twitter');
+const newQuoteBtnEl = document.getElementById('new-quote');
+var FontSize;
+(function (FontSize) {
+    FontSize["Long"] = "long-quote";
+})(FontSize || (FontSize = {}));
+let loading = false;
+let twitterQuote = '';
+let twitterQuoteAuthor = '';
+const setTweetQuote = (quote, author) => {
+    twitterQuote = quote;
+    twitterQuoteAuthor = author;
+};
+const tweetQuote = (quote, author) => {
+    const URL = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(URL, '_blank');
+};
+const renderText = (text = 'Unknown', el) => {
+    text.length > 150
+        ? el.classList.add(`${FontSize.Long}`)
+        : el.classList.remove(`${FontSize.Long}`);
+    el.textContent = text;
+};
 const getQuote = () => __awaiter(void 0, void 0, void 0, function* () {
     const proxyURL = 'https://cors-anywhere.herokuapp.com/';
     const apiURL = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
         const response = yield fetch(proxyURL + apiURL);
-        const data = yield response.json();
-        console.log(data);
+        const { quoteText, quoteAuthor } = yield response.json();
+        setTweetQuote(quoteText, quoteAuthor);
+        renderText(quoteText, quoteEl);
+        renderText(quoteAuthor, authorEl);
     }
     catch (error) {
         console.log(error);
     }
 });
+twitterBtnEl.addEventListener('click', () => tweetQuote(twitterQuote, twitterQuoteAuthor));
+newQuoteBtnEl.addEventListener('click', getQuote);
 getQuote();
--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,63 @@
-// Get quote form API
+// Elem refs
+const quoteContainerEl = document.getElementById(
+  'quote-container'
+) as HTMLElement;
+const quoteEl = document.getElementById('quote') as HTMLElement;
+const authorEl = document.getElementById('author') as HTMLElement;
+const twitterBtnEl = document.getElementById('twitter') as HTMLElement;
+const newQuoteBtnEl = document.getElementById('new-quote') as HTMLElement;
 
+// Style config
+enum FontSize {
+  Long = 'long-quote',
+}
+
+// State
+let loading = false;
+let twitterQuote = '';
+let twitterQuoteAuthor = '';
+
+const setTweetQuote = (quote: string, author: string): void => {
+  twitterQuote = quote;
+  twitterQuoteAuthor = author;
+};
+
+// Tweet quote
+const tweetQuote = (quote: string, author: string): void => {
+  const URL = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+  window.open(URL, '_blank');
+};
+
+// Render text in HTML elem
+const renderText = (text: string = 'Unknown', el: HTMLElement): void => {
+  text.length > 150
+    ? el.classList.add(`${FontSize.Long}`)
+    : el.classList.remove(`${FontSize.Long}`);
+
+  el.textContent = text;
+};
+
+// Get quote form API
 const getQuote = async (): Promise<void> => {
   const proxyURL = 'https://cors-anywhere.herokuapp.com/';
   const apiURL =
     'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
   try {
     const response = await fetch(proxyURL + apiURL);
-    const data = await response.json();
-    console.log(data);
+    const { quoteText, quoteAuthor } = await response.json();
+    setTweetQuote(quoteText, quoteAuthor);
+    renderText(quoteText, quoteEl);
+    renderText(quoteAuthor, authorEl);
   } catch (error) {
     console.log(error);
   }
 };
 
+// Event listeners
+twitterBtnEl.addEventListener('click', () =>
+  tweetQuote(twitterQuote, twitterQuoteAuthor)
+);
+newQuoteBtnEl.addEventListener('click', getQuote);
+
+// Invoke
 getQuote();
