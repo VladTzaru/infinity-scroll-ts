43f0c8fa91132a38086e92715e2bf9b37bdde1cf
VladTzaru <bvanja.87@gmail.com>
2020-12-07 15:08:30 +0100

    Added getQuote method

--- dist/app.js
+++ dist/app.js
@@ -1,5 +1,23 @@
 "use strict";
-var printWord = function (word) {
-    console.log(word);
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
 };
-printWord('Hello');
+const getQuote = () => __awaiter(void 0, void 0, void 0, function* () {
+    const proxyURL = 'https://cors-anywhere.herokuapp.com/';
+    const apiURL = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try {
+        const response = yield fetch(proxyURL + apiURL);
+        const data = yield response.json();
+        console.log(data);
+    }
+    catch (error) {
+        console.log(error);
+    }
+});
+getQuote();
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,16 @@
-const printWord = (word: string): void => {
-  console.log(word);
+// Get quote form API
+
+const getQuote = async (): Promise<void> => {
+  const proxyURL = 'https://cors-anywhere.herokuapp.com/';
+  const apiURL =
+    'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+  try {
+    const response = await fetch(proxyURL + apiURL);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-printWord('Hello');
+getQuote();
