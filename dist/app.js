6a24e31411813285921707fd7cfcb7df5cdd0c59
VladTzaru <bvanja.87@gmail.com>
2020-12-07 13:28:35 +0100

    Initial commit

---
+++ dist/app.js
@@ -0,0 +1,5 @@
+"use strict";
+var printWord = function (word) {
+    console.log(word);
+};
+printWord('Hello');
---
+++ src/app.ts
@@ -0,0 +1,5 @@
+const printWord = (word: string): void => {
+  console.log(word);
+};
+
+printWord('Hello');
